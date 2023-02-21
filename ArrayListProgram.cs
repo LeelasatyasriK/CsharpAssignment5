941c642d76a3e258db5d7a35cb200eeb5d281861
LeelasatyasriK <121661802+LeelasatyasriK@users.noreply.github.com>
2023-02-21 12:18:02 +0530

    Add files via upload

---
+++ ArrayListProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+namespace Asmt5SecondQuestion
+{
+    class ArrayListProgram
+    {
+        static void Main(string[] args)
+        {
+            ArrayList employee = new ArrayList();
+            employee.Add(new Employee { ID = 101, Name = "Leela", Department = "Developing" });
+            employee.Add(new Employee { ID = 102, Name = "Satya", Department = "Testing" });
+            employee.Add(new Employee { ID = 103, Name = "Sri", Department = "Finance" });
+            foreach (Employee emp in employee)
+            {
+                Console.Write(emp.ID + " ");
+                Console.Write(emp.Name+" ");
+                Console.WriteLine(emp.Department);
+            }
+        }
+    }
+}
